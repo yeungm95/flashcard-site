fb05ab64d573827127f102146a29af1e0fad512c
Michael Yeung <yeungm95@gmail.com>
2020-02-09 09:39:51 -0800

    create index , css, js, and readme files; create first working flashcard

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#well1").click(function() {
+    $(".term1").toggle();
+    $(".definition1").toggle();
+  })
+})
+
+$(document).ready(function() {
+  $("#well2").click(function() {
+    $(".term2").toggle();
+    $(".definition2").toggle();
+  })
+})
+
+
+
+
+// $(document).ready(function() {
+//   $("p").click(function() {
+//     $("img").show();
+//   });
+// });
+
