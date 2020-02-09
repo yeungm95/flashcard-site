652066b5e41087ef8efc75bfc40baf8cb5c8a1a2
Michael Yeung <yeungm95@gmail.com>
2020-02-09 11:43:17 -0800

    add all 19 term names and definitions to flash cards

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,82 @@ $(document).ready(function() {
   $("#well1").click(function() {
     $(".term1").toggle();
     $(".definition1").toggle();
-  })
-})
-
-$(document).ready(function() {
+  });
   $("#well2").click(function() {
     $(".term2").toggle();
     $(".definition2").toggle();
-  })
+  });
+  $("#well3").click(function() {
+    $(".term3").toggle();
+    $(".definition3").toggle();
+  });
+  $("#well4").click(function() {
+    $(".term4").toggle();
+    $(".definition4").toggle();
+  });
+  $("#well5").click(function() {
+    $(".term5").toggle();
+    $(".definition5").toggle();
+  });
+  $("#well6").click(function() {
+    $(".term6").toggle();
+    $(".definition6").toggle();
+  });7
+  $("#well7").click(function() {
+    $(".term7").toggle();
+    $(".definition7").toggle();
+  });
+  $("#well8").click(function() {
+    $(".term8").toggle();
+    $(".definition8").toggle();
+  });
+  $("#well9").click(function() {
+    $(".term9").toggle();
+    $(".definition9").toggle();
+  });
+  $("#well10").click(function() {
+    $(".term10").toggle();
+    $(".definition10").toggle();
+  });
+  $("#well11").click(function() {
+    $(".term11").toggle();
+    $(".definition11").toggle();
+  });
+  $("#well12").click(function() {
+    $(".term12").toggle();
+    $(".definition12").toggle();
+  });
+  $("#well13").click(function() {
+    $(".term13").toggle();
+    $(".definition13").toggle();
+  });
+  $("#well14").click(function() {
+    $(".term14").toggle();
+    $(".definition14").toggle();
+  });7
+  $("#well15").click(function() {
+    $(".term15").toggle();
+    $(".definition15").toggle();
+  });
+  $("#well16").click(function() {
+    $(".term16").toggle();
+    $(".definition16").toggle();
+  });
+  $("#well17").click(function() {
+    $(".term17").toggle();
+    $(".definition17").toggle();
+  });
+  $("#well18").click(function() {
+    $(".term18").toggle();
+    $(".definition18").toggle();
+  });
+  $("#well19").click(function() {
+    $(".term19").toggle();
+    $(".definition19").toggle();
+  });
 })
 
 
 
 
-// $(document).ready(function() {
-//   $("p").click(function() {
-//     $("img").show();
-//   });
-// });
 
